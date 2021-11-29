84c2a8c5a4544e6c351a1a0736f612b5845e934c
unrealbootpower <46008343+Rossi-Davide@users.noreply.github.com>
2021-11-29 22:38:19 +0100

    Fine sistema spawn blocchi

--- Assets/Scripts/GameObjectSpawner.cs
+++ Assets/Scripts/GameObjectSpawner.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class GameObjectSpawner : MonoBehaviour
 {
@@ -12,12 +13,12 @@ public class GameObjectSpawner : MonoBehaviour
     public GameObject spawner;
     public GameObject prefab;
 
-    private List<GameObject> instanciatedObjects;
+    private List<GameObject> instantiatedObjects;
 
 
     private void Start()
     {
-        instanciatedObjects = new List<GameObject>();
+        instantiatedObjects = new List<GameObject>();
     }
 
     public void BuildMap()
@@ -35,21 +36,64 @@ public class GameObjectSpawner : MonoBehaviour
 
         float lengthSingleSpace = spaceAvailableX / nSpaces;
 
+        float divisionsPerCube = BPM.Divisor;
+
+       
+
         spawner.GetComponent<RectTransform>().anchoredPosition = low.anchoredPosition;
 
-        Vector2 vector = new Vector2(0, (totalY / 2));
+        Vector2 vector;
+
+        if (divisionsPerCube >= 1)
+        {
+            vector = new Vector2(0, (totalY / (divisionsPerCube*2)));
+        }
+        else
+        {
+            vector = new Vector2(0, (totalY / 2));
+
+            divisionsPerCube = 1;
+        }
+
+       
 
         spawner.GetComponent<RectTransform>().anchoredPosition += vector;
 
         Vector2 singleSpace = new Vector2(lengthSingleSpace,0);
 
+        Vector2 height = new Vector2(0,totalY/divisionsPerCube);
+
+        Transform prefabTransform = prefab.GetComponent<Transform>();
+
+        Transform blockChild = prefabTransform.Find("block");
+
+        Vector3 blockChildNewScale = new Vector3(1/divisionsPerCube, blockChild.localScale.y, blockChild.localScale.z);
+
+        blockChild.localScale = blockChildNewScale;
+
         for(int i= 0; i<nCubes; i++)
         {
-
             spawner.GetComponent<RectTransform>().anchoredPosition += singleSpace;
-            ;
 
-            instanciatedObjects.Add(Instantiate(prefab, spawner.GetComponent<Transform>().position, prefab.GetComponent<Transform>().rotation, canvasParent));
+            if(divisionsPerCube != 1)
+            {
+                for (int j = 0; j < divisionsPerCube; j++)
+                {
+                    instantiatedObjects.Add(Instantiate(prefab, spawner.GetComponent<Transform>().position, prefab.GetComponent<Transform>().rotation, canvasParent));
+
+                    spawner.GetComponent<RectTransform>().anchoredPosition += height;
+                }
+
+
+
+                spawner.GetComponent<RectTransform>().anchoredPosition -= (height * divisionsPerCube);
+            }
+            else
+            {
+                instantiatedObjects.Add(Instantiate(prefab, spawner.GetComponent<Transform>().position, prefab.GetComponent<Transform>().rotation, canvasParent));
+            }
+            
+
 
         }
 
@@ -58,10 +102,12 @@ public class GameObjectSpawner : MonoBehaviour
 
     public void DestroyMap()
     {
-        foreach(GameObject g in instanciatedObjects)
+        foreach(GameObject g in instantiatedObjects)
         {
             Destroy(g);
-            instanciatedObjects.Remove(g);
+            
         }
+
+        instantiatedObjects.Clear();
     }
 }
